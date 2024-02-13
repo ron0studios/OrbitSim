ddd0e1e44631231c5f617edc6eeed71c6d5ce008
Ron0Studios <47331292+ron0studios@users.noreply.github.com>
2024-02-13 14:17:58 +0000

    Comment Simulator.cpp

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -10,7 +10,7 @@
 #include "ctpl.h"
 
 
-
+// initialises the simulation by creating a bounding box rectangle
 Simulator::Simulator(double bounds) {
     this->bounds = bounds;
 
@@ -20,30 +20,33 @@ Simulator::Simulator(double bounds) {
     boundbox.setOutlineThickness(100);
 }
 
+// appends a body object to the bodies list
 void Simulator::addBody(Body body) {
     bodies.push_back(body);
 }
 
-
+// destroys the current tree object and creates a new one
+// a mutex object is present to prevent race conditions with any of the
+// quad tree nodes which may end up getting shared
 void Simulator::updateTree(float brightness) {
     std::lock_guard<std::mutex> guard(mut);
 
     tree = QuadTree(this->bounds, &bodies, brightness);
-
-    //delQuadTree(&root);
-    //genQuadTree();
 }
 
-
+// updates all the forces for every body in the simulation
+// a mutex lock is present because there is some multithreading
+// operating on the bodies list which may lead to a race condition
+// crashing the program 
 void Simulator::updateForces(bool bruteForce) {
     std::lock_guard<std::mutex> guard(mut);
     maxForce = -1;
-    //std::cout << "loop\n" << std::endl;
 
-
+    // if the bruteforce parameter is set we still use multithreading.
+    // However, we update the forces without the barnes hut algorithm 
+    // but rather by looping through every body for each body, in an 
+    // O(N^2) algorithm. It is more accurate but painfully slow
     if(bruteForce) {
-
-
         int num_threads = 16;
         std::vector<std::thread> threads(num_threads);
 
@@ -66,43 +69,12 @@ void Simulator::updateForces(bool bruteForce) {
 
         for(int i = 0; i < num_threads; i++)
             threads[i].join();
-
-
-
-
-
-
-        /*
-        for (auto &bodyA: bodies) {
-            bodyA.acceleration = sf::Vector2<double>(0.0, 0.0);
-            for (auto &bodyB: bodies) {
-                if (&bodyA == &bodyB) continue;
-
-                bodyA.acceleration +=
-                        tree.forcePair(bodyA.mass, bodyB.mass, bodyA.position, bodyB.position) / bodyA.mass;
-
-            }
-            //std::cout << "\t" << bodyA.acceleration.x << " " << bodyA.acceleration.y << std::endl;
-        }
-         */
         return;
     }
 
-    /*
-    for(auto &body : bodies) {
-        tree.updateForce(&body, 0.5);
-    }
-     */
 
-
-    /*
-    for(int j = 0; j < bodies.size(); j++) {
-        tree.updateForce(&bodies[j], 0.5);
-        //maxForce = std::max(maxForce, bodies[j].mass * std::sqrt(std::pow(bodies[j].acceleration.x,2) + std::pow(bodies[j].acceleration.y,2)));
-        maxForce = std::max(maxForce, std::sqrt(std::pow(bodies[j].velocity.x,2) + std::pow(bodies[j].velocity.y,2)));
-    }
-     */
-
+    // if the bruteforce parameter is not set we use the quadtree to update
+    // each of the bodies using tree.updateForce
     int num_threads = 16;
     std::vector<std::thread> threads(num_threads);
 
@@ -111,7 +83,6 @@ void Simulator::updateForces(bool bruteForce) {
 
             for(int j = i*n; j < ((i+1)*n) and j < bodies.size(); j++) {
                 tree.updateForce(&bodies[j], 0.5);
-                //maxForce = std::max(maxForce, bodies[j].mass * std::sqrt(std::pow(bodies[j].acceleration.x,2) + std::pow(bodies[j].acceleration.y,2)));
                 maxForce = std::max(maxForce, std::sqrt(std::pow(bodies[j].velocity.x,2) + std::pow(bodies[j].velocity.y,2)));
             }
 
@@ -122,58 +93,22 @@ void Simulator::updateForces(bool bruteForce) {
 
     for(int i = 0; i < num_threads; i++)
         threads[i].join();
-
-
-
-    /*
-    for(auto &body : bodies) {
-        calcForce(body);
-    }
-     */
-
-
-    /*
-    int num_threads = 16;
-    std::vector<std::thread> threads(num_threads);
-
-    for(int i = 0; i < num_threads; i++){
-        threads[i] = std::thread([this](int i, int n){
-                for(int j = i*n; j < ((i+1)*n); j++)
-                    calcForce(bodies[j]);
-            },i, bodies.size()/num_threads);
-    }
-
-    for(int i = 0; i < num_threads; i++)
-        threads[i].join();
-
-        */
-
-
-
-
-
-    //for(auto & body : bodies) body.update(delta);
 }
 
+// draws each body by iterating through the bodies list
 void Simulator::draw(sf::RenderWindow& window) {
-    //window.draw(boundbox);
-    //root.draw(window);
-
-
     for(auto & body: bodies) {
         body.draw(window);
     }
 }
 
+// updates each body by iterating through the bodies list
 void Simulator::updateBodies(sf::Int64 delta) {
     std::lock_guard<std::mutex> guard(mut);
-    //double minforce = 0;
-    //for(auto & i : bodies)
-        //minforce = std::max(minforce, pow(i.acceleration.x,2) + pow(i.acceleration.y,2));
-    //std::cout << minforce << std::endl;
     for(auto & body : bodies) body.update(delta, maxForce);
 }
 
+// draws the quadtree by accessing the private tree variable
 void Simulator::drawTree(sf::RenderWindow &window) {
     std::lock_guard<std::mutex> guard(mut);
     tree.draw(window);

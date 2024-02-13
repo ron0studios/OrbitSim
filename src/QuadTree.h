a59ba7b50cedfc15057ea9595a59d921b2725fde
Ron0Studios <47331292+ron0studios@users.noreply.github.com>
2024-02-13 14:30:12 +0000

    Comment QuadTree.h

--- src/QuadTree.h
+++ src/QuadTree.h
@@ -27,25 +27,27 @@ class QuadTree {
         double bound;
         float brightness = 0.04;
 
-
+        // the node struct is declared as an attribute of the quadtree since a quadtree consists
+        // of several nodes, making accessing node properties more convenient. 
         struct node {
-            int child   = 0;
-            double mass = 0.0;
-            double width = 0.0;
+            int child   = 0; // the index of the first child of the node. children are stored contiguously
+            double mass = 0.0; // the total mass of the node and its children
+            double width = 0.0; // the width of the node
 
 
             // center of mass calculations
-            int total   = 0;
-            double massx = 0.0;
-            double massy = 0.0;
+            int total   = 0; // total bodies of the node
+            double massx = 0.0; // x position of center of mass
+            double massy = 0.0; // y position of center of mass
 
             // if node has 1 child
-            Body* singleChild = nullptr;
+            Body* singleChild = nullptr; // pointer to singleChild body
 
-            double cx = 0.0;
-            double cy = 0.0;
+            double cx = 0.0; // x position of the center of the node
+            double cy = 0.0; // y position of the center of the node
         };
 
+        // a list of nodes representing the quadtree
        std::vector<node> tree;
 };
 
